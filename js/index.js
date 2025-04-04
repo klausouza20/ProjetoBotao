ef07dce46c7b891f96184abdb049db8de2896172
klausouza20 <sousaklaudemar00@gmail.com>
2025-04-04 01:36:29 -0300

    Adicionando funcionalidade para mudar a cor de fundo

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
-function changeColor(){
-    let ccolor = document.getElementById("fundo");
-    ccolor.style.background = "black";
-    var textcolor = document.getElementById("text");
-    textcolor.style.color = "white";
-}
+function verificaCor(){
+    let colorc = document.getElementById("cor").value.toLowerCase();
+    const txt2 = document.getElementById("texto");
+    const cores = {
+        "vermelho":"red",
+        "verde":"green",
+        "azul":"blue",
+        "rosa":"pink",
+        "amarelo":"yellow"
+    };
+    let colorChange = document.getElementById("fundo");
 
+    if (cores[colorc]){
+        colorChange.style.backgroundColor = cores[colorc];
+        let txt = document.getElementById("text");
+        txt.style.color = "white";
+        txt2.innerHTML = ""
+    } else{
+        txt2.innerHTML = "Cor não reconhecida";
+    }
 
-function outColor(){
-    let ocolor = document.getElementById("fundo");
-    ocolor.style.background = "white";
-    var textcolor = document.getElementById("text");
-    textcolor.style.color = "black";
+    
 }
